aedfea5e5423f9c771dce7be33cf8a5fa5d9e8c1
xueaaaa <ss.bazunov@gmail.com>
2026-06-26 15:16:22 +0300

    add: link repository

--- internal/link/repository/link.go
+++ internal/link/repository/link.go
@@ -1,4 +1,36 @@
 package repository
 
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
 type LinkRepository interface {
+	Create(ctx context.Context, link LinkModel) error
+}
+
+type linkRepository struct {
+	db *pgx.Conn
+}
+
+func NewLinkRepository(db *pgx.Conn) LinkRepository {
+	return &linkRepository{
+		db: db,
+	}
+}
+
+func (r *linkRepository) Create(ctx context.Context, link LinkModel) error {
+	sql := `INSERT INTO links (shortId, origin, creationDate, lastAccessDate)
+			VALUES ($1, $2, $3, $4);`
+
+	_, err := r.db.Exec(
+		ctx,
+		sql,
+		link.ShortId,
+		link.Origin,
+		link.CreationDate,
+		link.LastAccessDate)
+
+	return err
 }
