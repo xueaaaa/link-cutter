38ce41ca886e90867600ab9c1dc699253d300553
xueaaaa <ss.bazunov@gmail.com>
2026-06-26 22:18:49 +0300

    add: create link handler

---
+++ internal/app/util/write_error.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"encoding/json"
+	errors2 "link-cutter/internal/app/errors"
+	"net/http"
+	"time"
+)
+
+func WriteError(w http.ResponseWriter, statusCode int, msg string) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	jsonErr := errors2.ErrorResponse{
+		Message:   msg,
+		Timestamp: time.Now().UTC(),
+	}
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "    ")
+	encoder.Encode(jsonErr)
+}
--- internal/link/service/link.go
+++ internal/link/service/link.go
@@ -7,6 +7,8 @@ import (
 	"link-cutter/internal/app/util"
 	"link-cutter/internal/link/repository"
 	"time"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type LinkService interface {
@@ -14,20 +16,33 @@ type LinkService interface {
 }
 
 type linkService struct {
-	repo repository.LinkRepository
+	repo     repository.LinkRepository
+	validate *validator.Validate
+}
+
+func NewLinkService(repo repository.LinkRepository) LinkService {
+	return &linkService{
+		repo:     repo,
+		validate: validator.New(),
+	}
 }
 
 func (s *linkService) Create(ctx context.Context, origin string) error {
 	link := repository.LinkModel{
-		Origin: origin,
+		Origin:       origin,
 		CreationDate: time.Now(),
 	}
 
 	for i := 0; i < 10; i++ {
 		shortId := util.GenerateShortId(8)
 		link.ShortId = shortId
-		err := s.repo.Create(ctx, link)
 
+		err := s.validate.StructCtx(ctx, link)
+		if err != nil {
+			return err
+		}
+
+		err = s.repo.Create(ctx, link)
 		if err == nil {
 			return nil
 		}
