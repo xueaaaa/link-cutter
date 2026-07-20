366886808373e3306b51611f02ff73c63dc312b6
xueaaaa <ss.bazunov@gmail.com>
2026-07-20 11:49:28 +0300

    add: optional auth middleware

--- internal/app/middleware/auth.go
+++ internal/app/middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"errors"
 	errors2 "link-cutter/internal/app/errors"
 	"link-cutter/internal/app/util"
 	"link-cutter/internal/user/model"
@@ -16,42 +17,35 @@ type contextKey string
 
 const userContextKey contextKey = "user"
 
+func parseToken(key string, r *http.Request) (*jwt.Token, model.Claims, error) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return nil, model.Claims{}, errors2.ErrMissingAuthHeader
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return nil, model.Claims{}, errors2.ErrInvalidAuthHeader
+	}
+
+	claims := model.Claims{}
+	token, err := jwt.ParseWithClaims(parts[1], &claims, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors2.ErrUnexpectedSigningMethod
+		}
+		return key, nil
+	})
+	if err != nil {
+		return nil, model.Claims{}, err
+	}
+	return token, claims, nil
+}
+
 func Auth(key string, logger *zap.Logger) func(handler http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			authHeader := r.Header.Get("Authorization")
-			if authHeader == "" {
-				logger.Info(errors2.ErrMissingAuthHeader.Error(),
-					zap.String("req_id", util.GetRequestId(r)),
-				)
-				util.WriteError(
-					w,
-					http.StatusUnauthorized,
-					errors2.ErrMissingAuthHeader.Error(),
-					util.GetRequestId(r),
-				)
-			}
+			token, claims, err := parseToken(key, r)
 
-			parts := strings.Split(authHeader, " ")
-			if len(parts) != 2 || parts[0] != "Bearer" {
-				logger.Info(errors2.ErrInvalidAuthHeader.Error(),
-					zap.String("req_id", util.GetRequestId(r)),
-				)
-				util.WriteError(
-					w,
-					http.StatusUnauthorized,
-					errors2.ErrInvalidAuthHeader.Error(),
-					util.GetRequestId(r),
-				)
-			}
-
-			claims := model.Claims{}
-			token, err := jwt.ParseWithClaims(parts[1], &claims, func(token *jwt.Token) (any, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, errors2.ErrUnexpectedSigningMethod
-				}
-				return key, nil
-			})
 			if err != nil || !token.Valid {
 				logger.Info(err.Error(),
 					zap.String("req_id", util.GetRequestId(r)),
@@ -66,6 +60,35 @@ func Auth(key string, logger *zap.Logger) func(handler http.Handler) http.Handle
 
 			ctx := context.WithValue(r.Context(), userContextKey, claims)
 			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func OptionalAuth(key string, logger *zap.Logger) func(handler http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, claims, err := parseToken(key, r)
+
+			switch {
+			case err == nil:
+				ctx := context.WithValue(r.Context(), userContextKey, claims)
+				next.ServeHTTP(w, r.WithContext(ctx))
+				return
+			case errors.Is(err, errors2.ErrMissingAuthHeader):
+				next.ServeHTTP(w, r)
+				return
+			default:
+				logger.Info(err.Error(),
+					zap.String("req_id", util.GetRequestId(r)),
+				)
+				util.WriteError(
+					w,
+					http.StatusUnauthorized,
+					err.Error(),
+					util.GetRequestId(r),
+				)
+				return
+			}
 		})
 	}
 }
